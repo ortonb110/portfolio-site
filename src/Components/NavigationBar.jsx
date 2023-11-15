55839a331ea4102e1533df2360417c57c1bd28c3
Orton Bright <ortonb110@gmail.com>
2023-11-15 22:43:19 +0000

    80% done - Navigation bar

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,7 +1,15 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
 const NavigationBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpen((open) => !open);
+  };
+
   return (
-    <nav className={`mobile md:tablet lg:desktop`}>
+    <nav className={`mobile md:tablet lg:desktop relative md:static`}>
       <div className="flex justify-between items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="61" height="32">
           <path
@@ -11,7 +19,13 @@ const NavigationBar = () => {
           />
         </svg>
 
-        <section className="hidden md:flex md:justify-between  md:gap-[4.2rem] uppercase text-[1.2rem] tracking-[2px]">
+        <section
+          className={`${
+            open
+              ? "max-md:small"
+              : "hidden"
+          }     hidden md:flex md:justify-between md:gap-[4.2rem] uppercase text-[1.2rem] tracking-[2px]`}
+        >
           <NavLink
             to={"/"}
             className={({ isActive }) => (isActive ? "text-primary" : "")}
@@ -31,7 +45,28 @@ const NavigationBar = () => {
             contact me
           </NavLink>
         </section>
-        <button className="md:hidden">menu</button>
+        <button
+          className={`md:hidden flex flex-col ${
+            open ? "gap-[0px]" : "gap-[4px]"
+          }`}
+          onClick={handleOpenMenu}
+        >
+          <span
+            className={`w-[3rem] h-[1px] bg-black inline-block ${
+              open ? "rotate-45" : "translate-y-0"
+            } anim`}
+          ></span>
+          <span
+            className={`w-[3rem] h-[1px] bg-black  anim ${
+              open ? "translate-x-[-10px] opacity-0 " : "inline-block"
+            } `}
+          ></span>
+          <span
+            className={`w-[3rem] h-[1px] bg-black inline-block ${
+              open ? "-rotate-45" : "translate-y-0"
+            } anim`}
+          ></span>
+        </button>
       </div>
     </nav>
   );
