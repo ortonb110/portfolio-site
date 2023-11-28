a25971c56712cd5922d9446e0a0b8beb1f13e674
Orton Bright <ortonb110@gmail.com>
2023-11-28 22:22:59 +0000

    Created sub footer component

---
+++ src/Components/SubFooter.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+
+const SubFooter = () => {
+  return (
+    <section className="mobile md:tablet xl:desktop flex flex-col justify-center md:flex-row md:justify-between md:items-center xl:space-x-[3.2rem] lg:space-x-[2.2rem] md:space-x-[1rem]">
+      <h2 className="md:w-[40%] ">Interested in doing a project together?</h2>
+      <span className="md:w-[30%] h-[1px] bg-[#979797] hidden md:inline-block">
+        
+      </span>
+    <NavLink to={'/contact'} className={`btnDefault border-primaryGreyishBlue hover:bg-primaryGreyishBlue hover:text-white`}>
+        contact me
+    </NavLink>
+    </section>
+  );
+};
+
+export default SubFooter;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Components/Hero"
 import About from "../Components/About"
+import SubFooter from "../Components/SubFooter"
 
 const Home = () => {
   return (
     <div className=''>
       <Hero/>
       <About/>
+      <SubFooter/>
     </div>
   )
 }
